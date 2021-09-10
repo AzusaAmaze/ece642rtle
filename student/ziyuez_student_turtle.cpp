8908cf403eecfd559e62188da45d03f595cc8e38
AzusaAmaze <azusaamaze@gmail.com>
2021-09-10 16:40:17 -0400

    put forward stepping into switch case, clean up bump test into helper

--- student/ziyuez_student_turtle.cpp
+++ student/ziyuez_student_turtle.cpp
@@ -2,9 +2,9 @@
  * Originally by Philip Koopman (koopman@cmu.edu)
  * and Milda Zizyte (milda@cmu.edu)
  *
- * STUDENT NAME:
- * ANDREW ID:
- * LAST UPDATE:
+ * STUDENT NAME: Ziyue ZHANG
+ * ANDREW ID: ziyuez
+ * LAST UPDATE: Sep 10, 2021
  *
  * This file is an algorithm to solve the ece642rtle maze
  * using the left-hand rule. The code is intentionaly left obfuscated.
@@ -19,12 +19,10 @@ turtleMove studentTurtleStep(bool bumped) {return MOVE;}
 // OK TO MODIFY BELOW THIS LINE
 
 #define TIMEOUT 40    // bigger number slows down simulation so you can see what's happening
-float w;
-float fx1, fy1, fx2, fy2;
-float bp, q;
-int state;
+int state, timer;     // global variables, shared for all functon calls overtime
 
-enum directions{left=0, up=1, right=2, down=3};
+enum directions{left=0, up=1, right=2, down=3};   // turtle directions
+
 
 /*
  * Helper function to turn turtle orientation to the right.
@@ -38,6 +36,7 @@ void turnRight(int& nw_or) {
   return;
 }
 
+
 /*
  * Helper function to turn turtle orientation to the left.
  * Input: nw_or turtle orientation
@@ -49,71 +48,91 @@ void turnLeft(int& nw_or) {
   else if (nw_or == down) nw_or = right;
   return;
 }
-		 
+
+
+/*
+ * Helper function to step forward the turtle by one unit.
+ * Input: pos_  turtle position
+ *        nw_or turtle orientation
+ */
+void stepForward(QPointF& pos_, int& nw_or) {
+  if (nw_or == up) pos_.setY(pos_.y() - 1); 
+  if (nw_or == right) pos_.setX(pos_.x() + 1);
+  if (nw_or == down) pos_.setY(pos_.y() + 1);
+  if (nw_or == left) pos_.setX(pos_.x() - 1);
+  return;
+}
+
+
+/*
+ * Helper function to test if turtle is bumped into wall.
+ * Input: pos_  turtle position
+ *        nw_or turtle orientation
+ */
+bool isBumped(QPointF& pos_, int& nw_or) {
+  float fx1, fy1, fx2, fy2;
+  fx1 = pos_.x(); 
+  fy1 = pos_.y();
+  fx2 = pos_.x(); 
+  fy2 = pos_.y();
+
+  if (nw_or == left) fy2 += 1;
+  else if (nw_or == up) fx2 += 1;
+  else if (nw_or == right) {
+    fx2 += 1;
+    fy2 += 1;
+    fx1 += 1;
+  } else if (nw_or == down) {
+    fx2 += 1;
+    fy2 += 1;
+    fy1 += 1;
+  }
+  return bumped(fx1,fy1,fx2,fy2);
+}
+
+
 // this procedure takes the current turtle position and orientation and returns
 // true=submit changes, false=do not submit changes
 // Ground rule -- you are only allowed to call the helper functions "bumped(..)" and "atend(..)",
 // and NO other turtle methods or maze methods (no peeking at the maze!)
 bool studentMoveTurtle(QPointF& pos_, int& nw_or) { 
-  ROS_INFO("Turtle update Called  w=%f", w);
+  ROS_INFO("Turtle update Called  timer=%f", timer);
 
   // if already reached destination
   if (atend(pos_.x(), pos_.y())) return false;
 
-  // when wait time is up, perform movement 
-  if(w == 0) {
-
-    // check whether turtle is facing a wall
-    fx1 = pos_.x(); 
-    fy1 = pos_.y();
-    fx2 = pos_.x(); 
-    fy2 = pos_.y();
-
-	  if (nw_or < 2) {
-  		if (nw_or == 0) fy2+=1;
-  		else            fx2+=1;
-    }
-		else{ 
-      fx2+=1; 
-      fy2+=1; 
-		  if (nw_or == 2) fx1+=1;  
-		  else            fy1+=1; 
-		}
-
-		bp = bumped(fx1,fy1,fx2,fy2);
-
+  // when timer is up, perform movement 
+  if(timer == 0) {
     /*
      * State of turtle is updated once with every call to studentTurtleStep, 
      * with the states in the "state machine" being: 
-     * 0. If bumped into wall, turn right. Else change to state 1.
+     * 0. If bumped into wall, turn right. Else step and change to state 1.
      * 1. Turn left and change to state 0.
      * State 0 is the entry point of this "state machine".
      */
-    ROS_INFO("old Orientation=%f  old STATE=%d", nw_or, state);
-
     switch (state) {
-      case 0: {
-        if (bp) turnRight(nw_or);
-        else state = 1;
+      case 0: 
+        if (isBumped(pos_, nw_or)) turnRight(nw_or);
+        else {
+          stepForward(pos_, nw_or);
+          state = 1;
+        }
         break;
-      }
-      case 1: { 
+      case 1: 
         turnLeft(nw_or);
         state = 0;
         break;
-      }
-      default: break;
+      default: 
+        break;
     }
 
 	  ROS_INFO("Orientation=%d  STATE=%d", nw_or, state);
-	  if(state == 1) {
-      if (nw_or == 1) pos_.setY(pos_.y() - 1); 
-      if (nw_or == 2) pos_.setX(pos_.x() + 1);
-      if (nw_or == 3) pos_.setY(pos_.y() + 1);
-      if (nw_or == 0) pos_.setX(pos_.x() - 1);
-    }
+
+    // update timer
+    timer = TIMEOUT;
+    return true;
+  } else {
+    timer -= 1;
+    return false;
   }
-  if (w==0) w  = TIMEOUT; else w -= 1;
-  if (w==TIMEOUT) return true;
-  return false;
 }
