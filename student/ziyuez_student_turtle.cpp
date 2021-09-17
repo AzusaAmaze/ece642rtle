62e34503d50b2d2f8dd063a2a6ebc7ffee25218e
AzusaAmaze <azusaamaze@gmail.com>
2021-09-17 10:04:50 -0400

    use enum & static for turtle state

--- student/ziyuez_student_turtle.cpp
+++ student/ziyuez_student_turtle.cpp
@@ -19,9 +19,9 @@ turtleMove studentTurtleStep(bool bumped) {return MOVE;}
 // OK TO MODIFY BELOW THIS LINE
 
 #define TIMEOUT 40    // bigger number slows down simulation so you can see what's happening
-int state, timer;     // global variables, shared for all functon calls overtime
 
-enum directions{left=0, up=1, right=2, down=3};   // turtle directions
+enum directions : int {left=0, up=1, right=2, down=3};   // turtle directions
+enum states {state_0, state_1};                          // turtle states
 
 
 /*
@@ -29,11 +29,22 @@ enum directions{left=0, up=1, right=2, down=3};   // turtle directions
  * Input: nw_or turtle orientation
  */
 void turnRight(int& nw_or) {
-  if (nw_or == left) nw_or = up;
-  else if (nw_or == up) nw_or = right;
-  else if (nw_or == right) nw_or = down;
-  else if (nw_or == down) nw_or = left;
-  return;
+  switch (nw_or) {
+    case(left): 
+      nw_or = up;
+      break;
+    case(up):
+      nw_or = right;
+      break;
+    case(right):
+      nw_or = down;
+      break;
+    case(down):
+      nw_or = left;
+      break;
+    default:
+      break;
+  }
 }
 
 
@@ -42,11 +53,22 @@ void turnRight(int& nw_or) {
  * Input: nw_or turtle orientation
  */
 void turnLeft(int& nw_or) {
-  if (nw_or == left) nw_or = down;
-  else if (nw_or == up) nw_or = left;
-  else if (nw_or == right) nw_or = up;
-  else if (nw_or == down) nw_or = right;
-  return;
+  switch (nw_or) {
+  case(left): 
+    nw_or = down;
+    break;
+  case(up):
+    nw_or = left;
+    break;
+  case(right):
+    nw_or = up;
+    break;
+  case(down):
+    nw_or = right;
+    break;
+  default:
+    break;
+  }
 }
 
 
@@ -91,11 +113,17 @@ bool isBumped(QPointF& pos_, int& nw_or) {
 }
 
 
-// this procedure takes the current turtle position and orientation and returns
-// true=submit changes, false=do not submit changes
-// Ground rule -- you are only allowed to call the helper functions "bumped(..)" and "atend(..)",
-// and NO other turtle methods or maze methods (no peeking at the maze!)
+/* 
+ * This procedure takes the current turtle position and orientation and returns
+ * true=submit changes, false=do not submit changes
+ * Input: pos_  turtle position
+ *        nw_or turtle orientation
+ */
 bool studentMoveTurtle(QPointF& pos_, int& nw_or) { 
+  static int timer = 0;
+  static states turtle_state = state_0;
+  bool time_up = (timer == 0);
+
   ROS_INFO("Turtle update Called  timer=%f", timer);
 
   // if already reached destination
@@ -110,29 +138,28 @@ bool studentMoveTurtle(QPointF& pos_, int& nw_or) {
      * 1. Turn left and change to state 0.
      * State 0 is the entry point of this "state machine".
      */
-    switch (state) {
-      case 0: 
+    switch (turtle_state) {
+      case state_0: 
         if (isBumped(pos_, nw_or)) turnLeft(nw_or);
         else {
           stepForward(pos_, nw_or);
-          state = 1;
+          turtle_state = state_1;
         }
         break;
-      case 1: 
+      case state_1: 
         turnRight(nw_or);
-        state = 0;
+        turtle_state = state_0;
         break;
       default: 
         break;
     }
 
-	  ROS_INFO("Orientation=%d  STATE=%d", nw_or, state);
+	  ROS_INFO("Orientation=%d  STATE=%d", nw_or, turtle_state);
 
     // update timer
     timer = TIMEOUT;
-    return true;
   } else {
     timer -= 1;
-    return false;
   }
+  return time_up;
 }
